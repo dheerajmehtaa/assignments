ba61917e972cd158b58678a8e945db1ccee66b82
dheerajmehta <86247811+dheerajmehtaa@users.noreply.github.com>
2021-10-21 00:09:46 +0530

    a

---
+++ importexport/swanand.we.session/importexport/swannand/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+
+	return `
+	<h3><a href = "index.html">home</a></h3>
+
+	<div id ="options">
+		<h3><a href = "jewellery.html">jewellery</a></h3>
+		<h3><a href = "electronics.html">Electronics</a></h3>
+		<h3><a href = "jewellery.html">Sign In </a></h3>
+		<h3><a href = "jewellery.html">Sign up</a></h3>
+
+	
+</div>`
+}
+
+export default navbar 
