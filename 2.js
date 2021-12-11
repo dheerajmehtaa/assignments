d7565d8dd7bd946030439835a3bd15e2f90469e3
dheerajmehta <86247811+dheerajmehtaa@users.noreply.github.com>
2021-12-11 23:31:06 +0530

    auth done

--- 2.js
+++ 2.js
@@ -1,40 +1,53 @@
-function runProgram(input) {
-	input = input.trim().split('\n');
-	var n = input[0];
-	var empty = []
+// function runProgram(input) {
+// 	input = input.trim().split('\n');
+// 	var n = input[0];
+// 	var empty = []
 	
-	var array = input[1].trim().split(' ');
-	 array.sort((a, b) => a - b);
-	// console.log(array);
-	// empty.sort((a, b) => (a - b))
-	for(var i = 0; i < n-1; i++) {
-		empty[i] = array[i+1] - array[i]
-		}
-		// var r = array[n-1] - array[0]
-		// empty.push(r)
+// 	var array = input[1].trim().split(' ');
+// 	 array.sort((a, b) => a - b);
+// 	// console.log(array);
+// 	// empty.sort((a, b) => (a - b))
+// 	for(var i = 0; i < n-1; i++) {
+// 		empty[i] = array[i+1] - array[i]
+// 		}
+// 		// var r = array[n-1] - array[0]
+// 		// empty.push(r)
 
-	console.log(empty);
+// 	console.log(empty);
 	
 	   
-	  }
-	  if (process.env.USER === "dheerajmehta") {
-		runProgram(`4
-		5 10 6 8`);
-	  } else {
-		process.stdin.resume();
-		process.stdin.setEncoding("ascii");
-		let read = "";
-		process.stdin.on("data", function (input) {
-		  read += input;
-		});
-		process.stdin.on("end", function () {
-		  read = read.replace(/\n$/, "");
-		  read = read.replace(/\n$/, "");
-		  runProgram(read);
-		});
-		process.on("SIGINT", function () {
-		  read = read.replace(/\n$/, "");
-		  runProgram(read);
-		  process.exit(0) ;
-		});
-	  }
+// 	  }
+// 	  if (process.env.USER === "dheerajmehta") {
+// 		runProgram(`4
+// 		5 10 6 8`);
+// 	  } else {
+// 		process.stdin.resume();
+// 		process.stdin.setEncoding("ascii");
+// 		let read = "";
+// 		process.stdin.on("data", function (input) {
+// 		  read += input;
+// 		});
+// 		process.stdin.on("end", function () {
+// 		  read = read.replace(/\n$/, "");
+// 		  read = read.replace(/\n$/, "");
+// 		  runProgram(read);
+// 		});
+// 		process.on("SIGINT", function () {
+// 		  read = read.replace(/\n$/, "");
+// 		  runProgram(read);
+// 		  process.exit(0) ;
+// 		});
+// 	  }
+
+var n = 15
+for(var i = 1; i <= n; i++) {
+if(i%3==0 && i % 5==0){
+	console.log("FizzBuzz")
+}else if(i%5==0){
+	console.log("Buzz")
+}else if (i % 3==0 ){
+	console.log("Fizz")
+}else{
+	console.log(i)
+}
+}
